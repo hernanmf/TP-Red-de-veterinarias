4801f41f306a3024fda78dd63548d849c4a8e3f6
rgasali <rgasali@gmail.com>
2022-11-08 19:08:13 -0300

    se carga veterinarias.ts. falta completarlo

---
+++ veterinarias.ts
@@ -0,0 +1,54 @@
+export default class Veterinaria {
+    private nombre : string; 
+    private sucursales:Array<Sucursal>;
+    private proveedores:Array<Proveedor>;
+
+    public constructor(nombre:string, sucursales:Array<Sucursal>,proveedores:Array<Proveedor>){
+        this.nombre = nombre;
+        this.sucursales=sucursales;
+        this.proveedores=proveedores;
+    }
+
+    
+
+    public getNombre() : string {
+        return this.nombre;
+    }
+
+    public setNombre(nuevoNombre: string ) : void {
+        this.nombre = nuevoNombre;
+    }
+
+    public getSucursal(){
+        
+    }
+    
+    public setSucursal(){
+        
+    }
+
+    public getProveedor(){
+
+    }
+
+    public setProveedor(){
+
+    }
+
+    public borrarSucursal(){
+
+    }
+
+    public borrarProveedor(){
+
+    }
+
+    public listarSucursal(){
+
+    }
+
+    public listarProveedor(){
+        
+    }
+
+}
