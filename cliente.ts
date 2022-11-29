a1b53122d18b8b4247f185f2e31123c4bb331017
ramiroqui <quinionerosramiro@gmail.com>
2022-11-29 19:07:50 -0300

    Gestor de archivos y base de datos

--- cliente.ts
+++ cliente.ts
@@ -1,20 +1,22 @@
-import {Paciente} from "./Paciente";
+import Paciente from "./Paciente";
 
 export default class Cliente{
     public id_Cliente : number;
     public nombre :  string;
     public telefono : number;
     public esVip : boolean;
-    public pacientes : Array<Paciente>;
     public cantidadVisitas :  number;
+    public pacientes : Array<Paciente>;
+    
 
-     public constructor(id_Cliente: number, nombre: string, telefono: number, esVip: boolean, pacientes: Array<Paciente>, cantidadVisitas: number) {
+     public constructor(id_Cliente: number, nombre: string, telefono: number, esVip: boolean, cantidadVisitas: number, pacientes: Array<Paciente>) {
         this.id_Cliente = id_Cliente;
         this.nombre = nombre;
         this.telefono = telefono;
         this.esVip = esVip;
-        this.pacientes = pacientes;
         this.cantidadVisitas = cantidadVisitas;
+        this.pacientes = pacientes;
+        
      }
      public getIdCliente(): number {
         return this.id_Cliente;
@@ -54,7 +56,7 @@ export default class Cliente{
      }
      public borrarPaciente(arregloPacientes: Array<Paciente>, nombrePaciente: string) {
          for (let i: number = 0; i <arregloPacientes.length; i++){
-            if (arregloPacientes[i].getNombre() == nombrePaciente){
+            if (arregloPacientes[i].getNombre() === nombrePaciente){
                arregloPacientes.splice(i, 1);
             }
          }
